3cd724a835a84aed9ca240b8948dcac6db433793
Nils Yuan <Nilsyuan@gmail.com>
2018-09-06 14:20:29 -0400

    I finished ex 2.

--- exercise2.rb
+++ exercise2.rb
@@ -1,7 +1,10 @@
 puts "#1 Total cost including tip of a $55 meal. 20% tip."
 puts 55 * 1.20
 
-puts "#2 String and integer"
+puts "#2 String and integer."
 puts "Tip at\n %" + 20.to_s
 
-puts "#3 #{45628*7839}"
+puts "#3 The total cost of the restaurant franchise around the globe is $#{45628*7839}."
+
+puts "#4 The answer is true."
+puts (10 < 20 && 30 < 20) || !(10 == 11)
